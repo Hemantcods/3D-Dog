e36246728eaf2ca08837056b4c33d2463095ee2a
Hemantcodes <hy564636@gmail.com>
2026-02-04 23:25:51 +0530

    added texture to dog using normal map

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,24 +1,39 @@
-import { OrbitControls,useGLTF } from '@react-three/drei'
-import { useThree } from '@react-three/fiber'
-import React from 'react'
+import { OrbitControls, useGLTF, useTexture } from "@react-three/drei";
+import * as THREE from "three";
+import { useThree } from "@react-three/fiber";
+import React from "react";
 const Dog = () => {
+  // with the canvas on the app.jsx we can use useThree hook now
 
-    // with the canvas on the app.jsx we can use useThree hook now 
+  useThree(({ camera, scene, gl }) => {
+    camera.position.z = 0.55;
+  });
 
-    useThree(({camera,scene,gl})=>{
-        console.log(camera.position)
-        camera.position.z=.55
-    })
-
-    const model=useGLTF("/models/dog.drc.glb")
-
+  const model = useGLTF("/models/dog.drc.glb");
+  const texure = useTexture({
+    normalMap: "/dog_normals.jpg",
+  });
+//   now the textrue auto flips the texture map so details are not good 
+  texure.normalMap.flipY=false
+  // now selecting dog to apply the texture
+  model.scene.traverse((child) => {
+    if (child.name.includes("DOG"))
+      child.material = new THREE.MeshMatcapMaterial({
+        normalMap: texure.normalMap,
+        color:0xff00000
+      });
+  });
   return (
     <>
-        <primitive object={model.scene} position={[.25,-0.55,0]} rotation={[0,Math.PI/4.3 ,0]} />
-        <directionalLight color={0xffffff} position={[0,5,5]} intensity={10} />
-        <OrbitControls/>
+      <primitive
+        object={model.scene}
+        position={[0.25, -0.55, 0]}
+        rotation={[0, Math.PI / 4.3, 0]}
+      />
+      <directionalLight color={0xffffff} position={[0, 5, 5]} intensity={10} />
+      <OrbitControls />
     </>
-  )
-}
+  );
+};
 
-export default Dog
+export default Dog;
