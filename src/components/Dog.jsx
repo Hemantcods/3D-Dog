93e4bcc666baa7293a882420200dcd0c14249bbd
Hemantcodes <hy564636@gmail.com>
2026-02-04 23:48:55 +0530

    improved texture

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,26 +1,42 @@
 import { OrbitControls, useGLTF, useTexture } from "@react-three/drei";
 import * as THREE from "three";
 import { useThree } from "@react-three/fiber";
-import React from "react";
+import { normalMap, texture } from "three/tsl";
 const Dog = () => {
   // with the canvas on the app.jsx we can use useThree hook now
 
   useThree(({ camera, scene, gl }) => {
     camera.position.z = 0.55;
+    // improving the colour sapce and render tech of opengl
+    ((gl.toneMapping = THREE.ReinhardToneMapping),
+      (gl.outputColorSpace = THREE.SRGBColorSpace));
   });
 
   const model = useGLTF("/models/dog.drc.glb");
+
+  /*
   const texure = useTexture({
     normalMap: "/dog_normals.jpg",
+    sampleMatCap:"/matcap/mat-2.png"
+  });*/
+  const [normalMap, sampleMatCap] = useTexture([
+    "/dog_normals.jpg",
+    "/matcap/mat-2.png",
+  ]).map((texture) => {
+      // now the textrue auto flips the texture map so details are not good
+      // now improve the colourspace of texture
+    texture.flipY = false, texture.colorSpace = THREE.SRGBColorSpace;
+    return texture
   });
-//   now the textrue auto flips the texture map so details are not good 
-  texure.normalMap.flipY=false
+
+
+
   // now selecting dog to apply the texture
   model.scene.traverse((child) => {
     if (child.name.includes("DOG"))
       child.material = new THREE.MeshMatcapMaterial({
-        normalMap: texure.normalMap,
-        color:0xff00000
+        normalMap: normalMap,
+        matcap: sampleMatCap,
       });
   });
   return (
