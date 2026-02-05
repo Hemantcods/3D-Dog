46a6f49936b5957d3aef2a398992a3ec6a706660
Hemantcodes <hy564636@gmail.com>
2026-02-05 18:49:39 +0530

    compelte basic structure of section 2

--- src/App.jsx
+++ src/App.jsx
@@ -76,7 +76,38 @@ function App() {
           <div className="first-line"></div>
           <div className="second-line"></div>
         </section>
-        <section id="section-2"></section>
+        <section id="section-2">
+          <div className="titles">
+            <div className="title">
+              <small>2020 - ONGOING</small>
+              <h1>Tommorowland</h1>
+            </div>
+            <div className="title">
+              <small>2018 - TODAY</small>
+              <h1>Navy Pier</h1>
+            </div>
+            <div className="title">
+              <small>2015 - TODAY</small>
+              <h1>MSI Chicago</h1>
+            </div>
+            <div className="title">
+              <small>2016</small>
+              <h1>This Was Louise's Phone</h1>
+            </div>
+            <div className="title">
+              <small>2012 - TODAY</small>
+              <h1>KIKK Festival 2018</h1>
+            </div>
+            <div className="title">
+              <small>2017</small>
+              <h1>The Kennedy Center</h1>
+            </div>
+            <div className="title">
+              <small>2016 - ONGOING</small>
+              <h1>Royal Opera Of Wallonia</h1>
+            </div>
+          </div>
+        </section>
         <section id="section-3"></section>
       </main>
     </>
